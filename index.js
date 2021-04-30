386448437f8bbde1cce83ee95300629532dc5a24
Duncan Magill <duncanmagill@hotmail.co.uk>
2021-04-30 17:24:11 +0100

    works, but a few bugs need fixed

--- index.js
+++ index.js
@@ -30,50 +30,51 @@ Instead of using hard-coded inputs, use prompts to grab:
 // => Action: replaces word to censor it the replacement string 
 // => Return: the new array if one was found, flase if string couldt be found 
 
-const text =
-  "Lorem, ipsum ipsum ipsum dolor sit amet, consectetur adipisicing elit, \
-  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi \
-  ut aliquip ex ea tempor commodo consequat. Duis aute irure dolor in reprehenderit \
-  in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
-  Excepteur sint occaecat tempor cupidatat non proident, \
-  sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-  let wordToCensor = "ipsum"
-  // let replacementString = "*****"
-// function censor(text, replacementString, wordToCensor) {
+function censor(text, replacementString, wordToCensor) {
   
   //✔ take the string, and put each word in an array called words
-  const words = text.split(' ')
-  console.log(`words: ${words}`)
+  const wordsArray = text.split(' ')
+  console.log(`words: ${wordsArray}`)
   
   //✔ check the array against wordToCensor, note down its index in an array, make a count of the words frequency
   let wordCount = 0
-  const wordIndexValue = []
 
-  for (word in words) {
+  for (let word in wordsArray) {
 
-    if (words[word] === wordToCensor) {
-
-      wordIndexValue.push(word)
-      console.log(`word Index Value: ${wordIndexValue}`)
+    if (wordsArray[word] === wordToCensor) {
+      
+      wordsArray.splice(word, 1, replacementString)
+      console.log(wordsArray)
 
       wordCount ++
 
       console.log(`count ${wordCount}`)
+
     }
     // if there has been no word counted, and we are on the last word in the array thn print there is no word
-    else if (wordCount === 0 && word === words[words.length-1]) {
+    else if (wordsArray[word] === wordsArray[wordsArray.length-1]) {
+      console.log()
+    }
+    else if (wordCount === 0 && wordsArray[word] === wordsArray[wordsArray.length-1]) {
       console.log(`There is no such word ☹`)
+      return false
     }
   
   }
-  // If that word === wordToCensor, replace that word with replacementString
+ }
 
+ const text =
+ "Lorem, ipsum ipsum ipsum dolor sit amet, consectetur adipisicing elit, \
+ sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+ Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi \
+ ut aliquip ex ea tempor commodo consequat. Duis aute irure dolor in reprehenderit \
+ in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
+ Excepteur sint occaecat tempor cupidatat non proident, \
+ sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-  // If word is not in the text, return false
-
-// }
+ 
+  censor(text, "******", "lndsnj")
 
 
 
